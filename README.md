124e70cccd07babcd97cd9ffc1c2988b89c0fc9e
wxy1105952676 <1105952676@qq.com>
2024-01-26 15:24:23 +0800

    # Init README

---
+++ README.md
@@ -0,0 +1,15 @@
+# Welcome to Code
+
+This is a repo for Well214 getting started with algorithm learning.
+
+
+Some code web：  
+https://sunnywhy.com  
+https://www.acwing.com  
+https://leetcode.cn/studyplan/top-100-liked
+
+```
+Hello World
+```
+
+
---
+++ 晴问/第 2 章 C&C++快速入门/2.1基本数据类型/Hello Sunny Why!.cpp
@@ -0,0 +1,17 @@
+/* 
+ * @Author: wxy
+ * @Date: 2024-01-26 15:14:41
+ * @LastEditors: wxy
+ * @LastEditTime: 2024-01-26 15:15:14
+ * @FilePath: \HelloCode\晴问\第 2 章 C&C++快速入门\2.1基本数据类型\Hello Sunny Why!.cpp
+ * @Description: 
+ * @
+ * @Copyright (c) 2024 by Author, All Rights Reserved. 
+*/
+
+#include <cstdio>
+
+int main() {
+    printf("Hello Sunny Why!");
+    return 0;
+}
